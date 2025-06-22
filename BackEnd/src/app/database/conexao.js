04d7d6d76921d5eaa2bcc0760f8b220be6cc68db
Caio Garcia dos Santos <100966478+CaioG4rcia@users.noreply.github.com>
2025-06-21 21:10:05 -0300

    feat: Configura conexão com DB usando variáveis de ambiente

--- BackEnd/src/app/database/conexao.js
+++ BackEnd/src/app/database/conexao.js
@@ -1,24 +1,27 @@
-import mysql from 'mysql'
+import mysql from 'mysql';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const connect = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    port:'3306',
-    password:'',
-    database:'users'
-})
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
 
-export const base = (sql, val = '', errorMessage) =>{
+export const base = (sql, val = '') => {
     return new Promise((resolve, reject) => {
-        connect.query(sql, val, (error, result)=>{
-            if(!resolve){
-                return reject(error, errorMessage)
+        connect.query(sql, val, (error, result) => {
+            if (error) {
+                console.error("Erro na consulta SQL:", error);
+                return reject(error);
+            } else {
+                return resolve(result);
             }
-            else{
-                return resolve(result)
-            }
-        })
-    })
-}
+        });
+    });
+};
 
-export default connect
+export default connect;
